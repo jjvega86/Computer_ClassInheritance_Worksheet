76305358ee8dfa1fca6ebab74854029732035a98
jjvega86 <jj.vega86@gmail.com>
2020-08-12 16:53:09 -0400

    Constructued computer class, plotted a BuildComputer method

--- FunWithClassesDay8/Computer.cs
+++ FunWithClassesDay8/Computer.cs
@@ -8,5 +8,23 @@ namespace FunWithClassesDay8
 {
     class Computer
     {
+        public Motherboard motherboard;
+        
+
+
+        public Computer()
+        {
+            CPU processor = new CPU("Intel", "i3");
+            RAM tempMemory = new RAM(2.0, "MemEx");
+            HardDrive storage = new HardDrive(250.0, 250.0);
+            GPU graphics = new GPU("BlazeGraphics", 2.0);
+            motherboard = new Motherboard("Dell", processor, tempMemory, storage, graphics);
+        }
+
+        public void BuildComputer()
+        {
+            //I want to prompt the user to build the computer using this method
+        }
+      
     }
 }
